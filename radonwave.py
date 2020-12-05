4b1508c6e5151638f1a38237a0858a333cfee311
Marcel Martin <marcel.martin@scilifelab.se>
2020-12-06 00:14:54 +0100

    Remove the second humidity column

--- radonwave.py
+++ radonwave.py
@@ -108,7 +108,7 @@ def main():
         except btle.BTLEException as e:
             print('Bluetooth error:', e, file=sys.stderr)
         else:
-            print('{time}\t{temperature:.2f}\t{humidity:.2f}\t{radon_avg}\t{radon_1day}\t{brightness}\t{accel:02X}\t{humidity2:.2f}'.format(
+            print('{time}\t{temperature:.2f}\t{humidity:.2f}\t{radon_avg}\t{radon_1day}\t{brightness}\t{accel:02X}'.format(
                 time=time.strftime('%Y-%m-%d %H:%M:%S'),
                 **vars(measurement)
                 ), sep='\t')
